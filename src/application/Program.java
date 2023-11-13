9629bb35e9897329efa469512a316379cd8e11fd
Joao Victor <vicjopers@gmail.com>
2023-11-13 14:56:29 -0300

    criando position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,42 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Position position = new Position(5, 5);
+		System.out.println(position);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 
 	}
 
