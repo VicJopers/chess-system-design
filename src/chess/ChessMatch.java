fe7f14d39fb75260bd76c7feb0f088ee3135d1c6
Joao Victor <vicjopers@gmail.com>
2023-11-16 16:29:27 -0300

    Criando as peças

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -11,6 +11,12 @@ public class ChessMatch {
 	}
 	public ChessPiece[][] getPieces(){
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
+		for(int i=0;i<board.getRows();i++) {
+			for(int j=0;i<board.getColumns();i++) {
+				mat[i][j] = (ChessPiece)board.pieces(i, j);
+			}
+		}
+		return mat;
 	}
 
 	
